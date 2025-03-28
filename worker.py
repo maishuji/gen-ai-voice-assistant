d89826e11cefd4c28bddd4526b70133e87a9cc6f
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-03-28 18:17:05 +0100

    Integrate OpenAI API

--- worker.py
+++ worker.py
@@ -30,4 +30,18 @@ def text_to_speech(text, voice=""):
 
 
 def openai_process_message(user_message):
-    return None
+    # Set the prompt for OpenAI Api
+    prompt = "Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations."
+    # Call the OpenAI Api to process our prompt
+    openai_response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo", 
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": user_message}
+        ],
+        max_tokens=4000
+    )
+    print("openai response:", openai_response)
+    # Parse the response to get the response message for our prompt
+    response_text = openai_response.choices[0].message.content
+    return response_text
