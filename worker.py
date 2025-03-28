a489ab363083f9961bfb956555728e52a6c4696f
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-03-28 18:16:09 +0100

    Add speech_to_text

--- worker.py
+++ worker.py
@@ -5,8 +5,25 @@ openai_client = OpenAI()
 
 
 def speech_to_text(audio_binary):
-    return None
-
+        
+    # Set up Watson Speech-to-Text HTTP Api url
+    base_url = '...'
+    api_url = base_url+'/speech-to-text/api/v1/recognize'
+    # Set up parameters for our HTTP reqeust
+    params = {
+        'model': 'en-US_Multimedia',
+    }
+    # Set up the body of our HTTP request
+    body = audio_binary
+    # Send a HTTP Post request
+    response = requests.post(api_url, params=params, data=audio_binary).json()
+    # Parse the response to get our transcribed text
+    text = 'null'
+    while bool(response.get('results')):
+        print('speech to text response:', response)
+        text = response.get('results').pop().get('alternatives').pop().get('transcript')
+        print('recognised text: ', text)
+        return text
 
 def text_to_speech(text, voice=""):
     return None
